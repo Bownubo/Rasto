2712f5d9a5bbb949199ac23ec8225f24b66c5542
Bownubo <stanw.vandenberg@gmail.com>
2017-01-20 12:35:50 +0100

    bugfix

--- website/php/index.php
+++ website/php/index.php
@@ -1,16 +1,14 @@
-
-<html lang="nl">
+<?php        ?>
 <head>
 <meta charset="utf-8">
 <meta name="description" content="de basispagina voor HTML">
 <meta name="Author" content="Stan van den berg">
 <link rel="stylesheet" type="text/css" href="../css/main.css">
-
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 <title>Bob autoverhuur</title>
 </head>
 <body>
-<section class=".textcontainer">
+<section class="textcontainer">
 <h1>Hello world</h1>
 </section>
 
