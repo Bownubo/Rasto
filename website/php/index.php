2871312b586a4210becc179deb673dbe9dc4d251
Bownubo <stanw.vandenberg@gmail.com>
2017-01-20 12:05:56 +0100

    lolololol

--- website/php/index.php
+++ website/php/index.php
@@ -4,15 +4,15 @@
 <meta charset="utf-8">
 <meta name="description" content="de basispagina voor HTML">
 <meta name="Author" content="Stan van den berg">
-<link rel="stylesheet" type="text/css" href="main.css">
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="../css/main.css">
+
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 <title>Bob autoverhuur</title>
 </head>
 <body>
-
-
-
+<section class=".textcontainer">
+<h1>Hello world</h1>
+</section>
 
 
 
