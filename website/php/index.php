4bb2e7c14c63525c91ce5a3a4be3fd98a9969b3d
Bownubo <stanw.vandenberg@gmail.com>
2017-01-20 11:02:40 +0100

    index.php

---
+++ website/php/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<html lang="nl">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="de basispagina voor HTML">
+<meta name="Author" content="Stan van den berg">
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="main.css">
+<title> voorbeeldpagina</title>
+</head>
+<body>
+
+</body>
+</HTML>
