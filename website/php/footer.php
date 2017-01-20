6002a800986898254b7a8fbc48abeb48b14e55f6
wout van kampen <wout van kampen>
2017-01-20 12:46:26 +0100

    footertje

--- website/php/footer.php
+++ website/php/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<h3>Locatie: Pieter de Hooghstraat 17   |   Postcode: 2215AL   |   Contact opnemen? <a class="footerlink" href="contact.php">Click hier</a></h3>
+Locatie: Pieter de Hooghstraat 17   |   Postcode: 2215AL   |   Contact opnemen? <a class="footerlink" href="contact.php">Click hier</a>
 </footer>
 
 
