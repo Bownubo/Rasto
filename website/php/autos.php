cec3cef1948dcbc10289c16e7638e06e4945516b
wout van kampen <wout van kampen>
2017-01-20 14:47:34 +0100

    ddsd

--- website/php/autos.php
+++ website/php/autos.php
@@ -1,10 +1,10 @@
 <?php include'header.php' ?>
   <link rel="stylesheet" href="../css/autos.css" />
 <main>
-<img src="../img/dummy1.png" alt="dummy foto 1" />
-<img src="../img/dummy2.png" alt="dummy foto 2" />
-<img src="../img/dummy4.png" alt="dummy foto 3" />
-<img src="../img/dummy3.png" alt="dummy foto 4" />
-
+  <img src="../img/dummy1.png" alt="dummy foto 1" />
+  <img src="../img/dummy2.png" alt="dummy foto 2" />
+  <div></div>
+  <img src="../img/dummy4.png" alt="dummy foto 3" />
+  <img src="../img/dummy3.png" alt="dummy foto 4" />
 </main>
 <?php include'footer.php' ?>
