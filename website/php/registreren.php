9a9e5a50b78c6cdd47e3f722850a101e4de7c075
Bownubo <stanw.vandenberg@gmail.com>
2017-01-27 11:36:05 +0100

    registeren html

---
+++ website/php/registreren.php
@@ -0,0 +1,36 @@
+<?PHP include 'header.php' ?>
+<section class="text-container">
+  <div class="reg-container">
+    <h4 class="floatrighty">
+      Maak nu een account!<br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra egestas aliquam. Morbi non risus vitae urna euismod aliquam sit amet eu est.
+
+    </h4>
+    <form method="get">
+      <table>
+        <tr><td>Gebruikersnaam</td><tr>
+        <tr><td><input type="text"></td></tr>
+        <tr><td>Wachtwoord</td></tr>
+        <tr><td><input type ="text"></td></tr>
+        <tr><td>Adres</td></tr>
+        <tr><td><input type ="text"></td></tr>
+        <tr><td>Postcode</td></tr>
+        <tr><td><input type ="text"></td></tr>
+        <tr><td>Telefoon nummer</td></tr>
+        <tr><td><input type ="text"></td></tr>
+
+      </table>
+    </form>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+<?PHP include 'footer.php' ?>
