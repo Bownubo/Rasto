e483f9740339d375f96c2492c4990d6a58fdd62f
Bownubo <stanw.vandenberg@gmail.com>
2017-01-27 08:58:21 +0100

    index pagina final version

--- website/php/index.php
+++ website/php/index.php
@@ -1,10 +1,16 @@
 <?PHP include 'header.php' ?>
 <body>
   <section class="banner-text-container">
-    <p id="autobannertext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed cursus dolor</P>
+    <p id="autobannertext">Waarom bob autoverhuur<br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in dolor nec dui semper maximus. Nullam at porttitor arcu. Phasellus porta orci nec felis rutrum, eget faucibus erat luctus. Sed vehicula diam in nibh cursus, ut varius tortor dapibus. Integer vel sodales nunc. Fusce bibendum velit vitae posuere congue. Suspendisse pellentesque, lacus a mo
+    </P>
   </section>
   <section class="text-container">
-  </section>
-</div>
-</body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in dolor nec dui semper maximus. Nullam at porttitor arcu. Phasellus porta orci nec felis rutrum, eget faucibus erat luctus. Sed vehicula diam in nibh cursus, ut varius tortor dapibus. Integer vel sodales nunc. Fusce bibendum velit vitae posuere congue. Suspendisse pellentesque, lacus a molestie aliquet, velit leo tempor diam, et dignissim ipsum velit quis odio. Curabitur eu cursus lorem. Nulla facilisis consequat dapibus.</p>
+
+    <p>Morbi tincidunt, massa vel blandit eleifend, augue urna cursus arcu, sit amet ornare neque turpis eget quam. Duis ornare egestas accumsan. Fusce varius auctor lorem ac dignissim. Quisque accumsan vel metus in gravida. Aenean sed molestie nibh. Quisque et imperdiet neque. Donec diam arcu, sagittis sed ligula in, rhoncus bibendum velit. Curabitur ex purus, condimentum eu mi a, accumsan ultricies ligula. Quisque porttitor urna quam, non lacinia nunc auctor vel. Ut placerat velit nec mollis mattis.</p>
+    dolor sit amet, consectetur adipiscing elit. Maecenas auctor egestas sapien sit amet varius. Nullam venenatis mauris at laoreet malesuada. Quisque accumsan, eros eu luctus ultricies, mi nisl auctor ex, nec dignissim arcu elit id urna. Proin eleifend nunc imperdiet, convallis metus ac, lobortis tellus. Nam egestas, tortor nec rhoncus auctor, sem lacus pellentesque nisi, eu cursus dolor odio in lectus. Donec id mattis urna, quis laoreet lacus. In nec tempor purus. Sed ullamcorper vehicula egestas. Mauris consequat massa vel lacinia interdum. Aliquam sit amet lorem ut lectus venenatis fringilla ullamcorper non sapien. Nullam vitae elit venenatis, scelerisque lectus eget, rhoncus augue. Phasellus nec eleifend arcu. Etiam feugiat nulla ut erat aliquam tempus. Duis lorem leo, sodales sit amet purus eget, pulvinar fermentum ipsum.
+
+    <p>Aliquam non leo faucibus, pretium nibh eget, scelerisque turpis. Etiam lacus lectus, venenatis in interdum at, feugiat condimentum libero. Fusce eget arcu sit amet lacus bibendum condimentum. Aenean vulputate diam quis fringilla pretium. Mauris ut interdum augue, in porta est. Pellentesque eu nulla id nulla malesuada luctus quis vitae purus. Maecenas et mi nisi. Nunc ultricies quam ut egestas laoreet. Quisque maximus, turpis non pulvinar hendrerit, diam lacus feugiat quam, id finibus ipsum velit eget augue.
+  Vestibulum et enim facilisis, efficitur justo sed, pellentesque felis. Sed convallis iaculis felis, vius leo fringilla, tristique neque in, venenatis felis.
 <?PHP include 'footer.php' ?>
