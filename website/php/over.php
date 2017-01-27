6c5b913e38e3c29f43daee52e002d6e321f63ed7
Kevin <kevinvdh04@gmail.com>
2017-01-27 09:19:51 +0100

    over ons pagina af

--- website/php/over.php
+++ website/php/over.php
@@ -1,8 +1,16 @@
 <?PHP include 'header.php'?>
 <head>
-    <link ref="stylesheet" src"../css/over.css">
+    <link rel="stylesheet" href="../css/over.css">
 </head>
 <body>
-    
-
+    <div class="body">
+        <p class="tekst1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor magna et sodales pretium. Donec sit amet lectus libero. Proin sodales auctor sagittis. Ut in magna eu erat suscipit fringilla sed sed risus. Praesent rhoncus, velit maximus dapibus elementum, risus leo dictum mauris, ultrices pellentesque purus ipsum at leo. Proin pretium odio in turpis finibus, id mattis lectus luctus. Sed efficitur et turpis non volutpat. Vivamus vitae ipsum laoreet, sodales nulla ut, porttitor libero. Aliquam quis rutrum ligula. Suspendisse pharetra dui aliquam ornare bibendum. Fusce tincidunt venenatis odio, et tincidunt dolor placerat non. Morbi mollis sapien at tempus elementum. Praesent a fermentum diam. Sed ut metus a lectus pharetra maximus in sit amet libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin gravida varius iaculis. Mauris nec nibh non sapien ullamcorper dignissim vitae in felis. Donec nibh ipsum, ullamcorper vitae malesuada at, cursus sed arcu. Nulla in massa orci. Sed tellus lacus, luctus at augue ac, eleifend lobortis erat. Vestibulum interdum felis vitae odio congue, in consequat purus accumsan. Nullam rutrum dui lectus, id gravida eros pellentesque id.
+        </p>
+        <img class="auto" src="../img/Sports-Cars-Wallpapers.jpg" alt="rode auto"/>
+        <img class="auto" src="../img/7482.png"alt="zwarte auto"/>
+        <p class="tekst2">
+        Mauris ultrices aliquam augue sit amet sodales. Integer condimentum sem hendrerit justo semper mollis. Ut imperdiet a ligula non fringilla. Fusce semper auctor erat, eu interdum justo vulputate sit amet. Integer tristique augue risus, eget sagittis diam sagittis ut. Nullam maximus eu nibh sit amet iaculis. Quisque congue, arcu vitae rutrum vulputate, mi nulla facilisis ipsum, sed rhoncus elit quam ac velit. Nam efficitur libero eget libero commodo pellentesque. Vivamus sit amet vulputate magna, eget mattis purus. Nunc suscipit nec tortor ac eleifend. Donec viverra gravida metus, nec egestas tortor consequat ullamcorper. Cras tristique, erat quis dignissim volutpat, neque dui varius massa, sed luctus magna ante at dolor. Mauris volutpat odio in magna luctus, eu laoreet lorem laoreet. Aenean quam mauris, pellentesque a felis eu, consectetur volutpat nunc. Sed porta risus euismod lectus pellentesque ornare mattis at risus. Morbi aliquet tincidunt malesuada. Proin sed risus rhoncus, pulvinar enim in, auctor felis. Integer non sapien iaculis, condimentum magna non, pharetra dolor. Sed gravida odio sed nisl luctus, sit amet scelerisque ligula egestas. Morbi justo diam, volutpat eu enim in, bibendum vehicula orci.
+        </p>
+    </div>
 <?PHP include 'footer.php'?>
