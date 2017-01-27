7b64ad65ca22ca58de57e55903f07f1e667140c5
Kevin <kevinvdh04@gmail.com>
2017-01-27 11:43:56 +0100

    de contactpagina

--- website/php/contact.php
+++ website/php/contact.php
@@ -1,7 +1,37 @@
-<?php include "header.php"?>
+<?php include "header.php" ?>
 <head>
     <link rel="stylesheet" href="../css/contact.css">
 </head>
 <body>
-
-<?php include "footer.php"?>
+    <div class="body">
+        <form action="demo_form.asp">
+            <table>
+                <tr>
+                    <td>Voornaam</td><td class="message">Message</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="voornaam"></td><td rowspan="5"><textarea rows="7" cols="50"></textarea></td>
+                </tr>
+                <tr>
+                    <td>Achternaam</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="achternaam"></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="email"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Submit"></td>
+                </tr>
+            </table>
+        </form>
+        <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. hendrerit pretium tellus, vel faucibus velit porttitor non.
+        </h1>
+        <iframe src="https://maps.google.com/maps?q=LeidenLammenschans,Leiden,Nederland,&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+    </div>
+<?php include "footer.php" ?>
--- website/php/over.php
+++ website/php/over.php
@@ -1,4 +1,4 @@
-<?PHP include 'header.php'?>
+<?PHP include 'header.php' ?>
 <head>
     <link rel="stylesheet" href="../css/over.css">
 </head>
@@ -13,4 +13,4 @@
         Mauris ultrices aliquam augue sit amet sodales. Integer condimentum sem hendrerit justo semper mollis. Ut imperdiet a ligula non fringilla. Fusce semper auctor erat, eu interdum justo vulputate sit amet. Integer tristique augue risus, eget sagittis diam sagittis ut. Nullam maximus eu nibh sit amet iaculis. Quisque congue, arcu vitae rutrum vulputate, mi nulla facilisis ipsum, sed rhoncus elit quam ac velit. Nam efficitur libero eget libero commodo pellentesque. Vivamus sit amet vulputate magna, eget mattis purus. Nunc suscipit nec tortor ac eleifend. Donec viverra gravida metus, nec egestas tortor consequat ullamcorper. Cras tristique, erat quis dignissim volutpat, neque dui varius massa, sed luctus magna ante at dolor. Mauris volutpat odio in magna luctus, eu laoreet lorem laoreet. Aenean quam mauris, pellentesque a felis eu, consectetur volutpat nunc. Sed porta risus euismod lectus pellentesque ornare mattis at risus. Morbi aliquet tincidunt malesuada. Proin sed risus rhoncus, pulvinar enim in, auctor felis. Integer non sapien iaculis, condimentum magna non, pharetra dolor. Sed gravida odio sed nisl luctus, sit amet scelerisque ligula egestas. Morbi justo diam, volutpat eu enim in, bibendum vehicula orci.
         </p>
     </div>
-<?PHP include 'footer.php'?>
+<?PHP include 'footer.php' ?>
