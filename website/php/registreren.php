1cc41f91a867bc727acf340a3772545ce8f4ca55
Kevin <kevinvdh04@gmail.com>
2017-02-17 11:53:04 +0100

    kanker

--- website/php/registreren.php
+++ website/php/registreren.php
@@ -1,12 +1,16 @@
 <?PHP include 'header.php' ?>
+<head>
+     <link rel="stylesheet" type="text/css" href="../css/registreren.css">
+</head>
 <section class="text-container">
   <div class="reg-container">
-    <h4 class="floatrighty">
-      Maak nu een account!<br>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra egestas aliquam. Morbi non risus vitae urna euismod aliquam sit amet eu est.
-
+    <h4>
+      Maak nu een account!
     </h4>
-    <form method="get">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra egestas aliquam. Morbi non risus vitae urna euismod aliquam sit amet eu est.
+    </p>
+    <form method="get" action="aanmelden.php">
       <table>
         <tr><td>Gebruikersnaam</td><tr>
         <tr><td><input type="text"></td></tr>
@@ -16,21 +20,11 @@
         <tr><td><input type ="text"></td></tr>
         <tr><td>Postcode</td></tr>
         <tr><td><input type ="text"></td></tr>
-        <tr><td>Telefoon nummer</td></tr>
+        <tr><td>Telefoonnummer</td></tr>
         <tr><td><input type ="text"></td></tr>
-
+        <tr><td><input class="button" type="submit" value="Aanmelden"></input></td></tr>
       </table>
     </form>
   </div>
 </section>
-
-
-
-
-
-
-
-
-
-
 <?PHP include 'footer.php' ?>
