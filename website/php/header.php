9d591aebd36fcad361b75ffadc31c38fd27527d0
wout van kampen <wout van kampen>
2017-03-10 12:14:09 +0100

    retard

--- website/php/header.php
+++ website/php/header.php
@@ -1,44 +1,22 @@
 <?php
-if (isset($_POST['reg'])) {
-  $dsn = 'mysql:host=localhost;dbname=autoverhuurbob';
-  $userdb = "root";
-  $passdb = "";
-  /*
-  *Create connection
-  */
-  $conn = new PDO($dsn,$userdb,$passdb);
-  $username = $_POST['username'];
-  $pass = $_POST['password'];
-  $email = $_POST['email'];
-  /*
-  *Hasing password
-  */
-  $password = md5($pass);
-  /*
-  *Prepare SQL statement
-  */
-  $db = $conn->prepare("INSERT INTO users (Username,Password,Email) VALUES (?,?,?)");
-  $db->execute(array($username, $password, $email));
-}
-if (isset($_POST['login'])) {
-  $dsn = 'mysql:host=localhost;dbname=autoverhuurbob';
-  $userdb = "root";
-  $passdb = "";
-  $conn = new PDO($dsn,$userdb,$passdb);
-  $username = $_POST['username'];
-  $pass = $_POST['password'];
 
-  /*
-  *hasing password
-  */
-  $password = md5($pass);
-  $db = $conn->prepare("SELECT * FROM users WHERE Username = ? or Email = ? AND Password = ?");
-  $db->execute(array($username, $username, $pass));
-  $result = $db->fetch(PDO::FETCH_ASSOC);
-  if ($result) {
-    echo "kaas";
-  }
+//verbinden met database
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "autoverhuurbob";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+//test of de verbinding is gelukt
+if (mysqli_connect_errno()) {
+die("verbinden met de database is mislukt" .
+mysqli_connect_error() .
+" (" . mysqli_connect_errno() . " )"
+);
 }
+
+//sluit de db verbinding
+mysqli_close($connection);
  ?>
 <html>
 <head>
