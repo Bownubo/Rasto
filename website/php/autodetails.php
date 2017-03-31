a1a8803631e56b73023e1dd939ec4487a270c177
Kevin <kevinvdh04@gmail.com>
2017-03-31 14:26:31 +0200

    our suffering has ended

--- website/php/autodetails.php
+++ website/php/autodetails.php
@@ -8,15 +8,14 @@ $query = "SELECT auto, autonaam, prijs, kenteken, autoid, beschikbaar FROM autos
           if ($result = mysqli_query($connection, $query)){
               while($row = mysqli_fetch_row($result)){
                   echo "<img class='autos-img' src='../img/".$row[0]."'><p>Naam: ".$row[1]."<br/> prijs per maand: ".$row[2]."<br/> kenteken: ".$row[3]."
-                  <br/>".$row[5];
+                  <br/>Aantal beschikbare auto's: ".$row[5]."
+                  <form action='huren.php' method='get'>
+                  <button type='submit' value='".$row[4]."' name='id'> huur auto!</button>
+                  </form>";
               }
           }
       }
           else{ die('je bent niet ingelogd');}
 ?>
-
-<form action="" method="get">
-<button type="submit" value="huur auto!" name="minder" >huur auto!</button>
-</form>
 </section>
 <?php include'footer.php' ?>
--- website/php/huren.php
+++ website/php/huren.php
@@ -1,7 +1,13 @@
-<?php include 'header.php' ?>
+<?php include 'header.php';
+if(isset($_SESSION['Gebruikersnaam'])){
+ $query = "UPDATE autos SET beschikbaar = beschikbaar -1
+WHERE autoid=".$_GET['id'];
 
-
-
-
-
+mysqli_query($connection, $query);
+}
+?>
+<section class="text-container">
+    <h1 style="padding-top:40px; margin:0; text-align:center;"> Je auto is gehuurd</h1>
+    <img style="margin:0 auto; display:block;" width="400px" src="../img/smiley.png" />
+</section>
 <?php include'footer.php' ?>
